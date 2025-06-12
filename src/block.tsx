045302bde875d50a7aba3a020c2579162de732b8
fabiensabatie <fabiensabatie@users.noreply.github.com>
2025-06-12 16:40:39 +0200

    🎉 Initialize block from default-webpack template

---
+++ src/block.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+interface BlockProps {
+}
+
+const Block: React.FC<BlockProps> = ({}) => {
+  
+  return (
+    <div>
+      Hello Block!
+    </div>
+  );
+};
+
+export default Block;
